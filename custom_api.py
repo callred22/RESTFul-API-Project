aea13171a421b90fe36a3e0409bb7add526ae768
callred22 <156968639+callred22@users.noreply.github.com>
2024-04-19 22:17:04 -0400

    Add files via upload

--- custom_api.py
+++ custom_api.py
@@ -2,6 +2,18 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+# API URLS
+api_url_med = "https://data.cms.gov/data-api/v1/dataset/d7fabe1e-d19b-4333-9eff-e80e0643f2fd/data"
+api_url_zip = "https://raw.githubusercontent.com/millbj92/US-Zip-Codes-JSON/master/USCities.json"
+api_url_cov = "https://data.cms.gov/data-api/v1/dataset/2684c3e2-3598-4997-a598-0991bad6fbf2/data"
+
+# Fetch/Driver File
+from driver import fetch_data_from_api
+
+# Random Functions
+from functions import FixWholeState
 
 #define application and database variables
 app = Flask(__name__)
@@ -11,49 +23,96 @@ db = SQLAlchemy(app)
 app_version = "v1/"
 
 #create the data definition
-class ZipModel(db.model):
+class ZipModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     zip = db.Column(db.String(100), nullable=False)
     state = db.Column(db.String(100), nullable=False)
 
-class MedicareModel(db.model):
+class MedicareModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     state = db.Column(db.String(100), nullable=False)
-    medicareAmount = db.Column(db.Integer, nullable=False)
+    totalEnrollments = db.Column(db.Integer, nullable=False)
 
-class CovidModel(db.model):
+class CovidModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     state = db.Column(db.String(100), nullable=False)
     hospitalizations = db.Column(db.Integer, nullable=False)
 
-class UserModel(db.model):
+class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(200), nullable=False)
     state = db.Column(db.String(100), nullable=False)
     zip = db.Column(db.String(100), nullable=False)
 
-
-
-# class VideoModel(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     views = db.Column(db.Integer, nullable=False)
-#     likes = db.Column(db.Integer, nullable=False)
-
 #outputs to log/screen to verify data visually
 def __repr__(self):
     return f"Info(avgMedicare = {avgMedicare}, avgCovidHosp = {avgCovidHosp}, state = {state})"
-    # return f"Video(name = {name}, views = {views}, likes = {likes})"
 
-#run this statement the first thme to create the database structure
-#db.create_all()
+# CHECKS to see if there is a db, if there is not it creates one
+if os.path.isfile('./database.db'):
+    print("\033[92m {}\033[00m" .format("Database connected..."))
+    pass
+elif not os.path.isfile('./database.db'):
+    print("\033[93m {}\033[00m" .format("Database not found... creating..."))
+    db.create_all()
+
+
+# PRE-FILL relevant data in db
+
+zip_db_data = ZipModel.query.all()
+if len(zip_db_data) < 1:
+    zip_data = fetch_data_from_api(api_url_zip)
+
+    # Zipcode array to be saved to db
+    new_zips = []
+    for i in zip_data:
+        if i['zip_code'] < 1000:
+            raw_zip = "00" + str(i['zip_code'])
+        elif i['zip_code'] < 10000:
+            raw_zip = "0" + str(i['zip_code'])
+        else:
+            raw_zip = str(i['zip_code'])
+
+        new_zip_obj = ZipModel(zip= raw_zip, state = i['state'])
+        new_zips.append(new_zip_obj)
+    
+    db.session.add_all(new_zips)
+    db.session.commit()
+    print("\033[92m {}\033[00m" .format("Finished inserting zipcode data"))
+
+cov_db_data = CovidModel.query.all()
+if len(cov_db_data) < 1:
+    cov_data = fetch_data_from_api(api_url_cov)
+
+    # Zipcode array to be saved to db
+    new_covs = []
+    for i in cov_data:
+        # Check to see if the state is in the functions file
+        if FixWholeState(i['Bene_Geo_Desc']) != "NO STATE":
+            new_cov_obj = CovidModel(state = FixWholeState(i['Bene_Geo_Desc']), hospitalizations = i['Total_Hosp'])
+            new_covs.append(new_cov_obj)
+    
+    db.session.add_all(new_covs)
+    db.session.commit()
+    print("\033[92m {}\033[00m" .format("Finished inserting Covid data"))
+
+med_db_data = MedicareModel.query.all()
+if len(med_db_data) < 1:
+    med_data = fetch_data_from_api(api_url_med)
+
+    # Zipcode array to be saved to db
+    new_meds = []
+    for i in med_data:
+        new_med = MedicareModel(state = i['BENE_STATE_ABRVTN'], totalEnrollments = i['TOT_BENES'])
+        new_meds.append(new_med)
+    
+    db.session.add_all(new_meds)
+    db.session.commit()
+    print("\033[92m {}\033[00m" .format("Finished inserting Medicare data"))
+
 
 #handle the incoming data request with a parser
 #arguments for a put request
-# video_put_args = reqparse.RequestParser()
-# video_put_args.add_argument("name", type=str, help="Name of the video is required",required=True)
-# video_put_args.add_argument("views", type=int, help="Views of the video",required=True)
-# video_put_args.add_argument("likes", type=int, help="Likes on the video",required=True)
 
 user_put_args = reqparse.RequestParser()
 user_put_args.add_argument("user", help="User pincode", required=True)
@@ -61,23 +120,11 @@ user_put_args.add_argument("state", help="User state", required=True)
 user_put_args.add_argument("zip", help="User zipcode", required=True)
 
 #arguments for an update request
-# video_update_args = reqparse.RequestParser()
-# video_update_args.add_argument("name", type=str, help="Name of the video is required")
-# video_update_args.add_argument("views", type=int, help="Views of the video")
-# video_update_args.add_argument("likes", type=int, help="Likes on the video")
 
 user_update_args = reqparse.RequestParser()
 user_update_args.add_argument("user", help="User pincode", required=True)
 user_update_args.add_argument("state", help="User state", required=False)
 user_update_args.add_argument("zip", help="User zipcode", required=False)
-
-#Map the types to columns extracted from the database object
-# resource_fields = {
-#     'id': fields.Integer,
-#     'name': fields.String,
-#     'views': fields.Integer,
-#     'likes': fields.Integer
-# }
 
 resource_fields = {
     'id': fields.Integer,
@@ -85,56 +132,6 @@ resource_fields = {
     'state': fields.String,
     'zip': fields.String
 }
-
-
-#Set up the Resource Functions for CRUD
-# class Video(Resource):
-#     #GET (READ in CRUD)
-#     #@marshal_with serializes output from the DB as a dictionary (json object) so we can work with it in python
-#     @marshal_with(resource_fields)
-   
-#     def get(self, video_id):
-#         result = VideoModel.query.filter_by(id=video_id).first()
-#         if not result:
-#             abort(404, message="Could not find video with that id")
-#         return result, 200
-
-#     #POST (CREATE in CRUD)
-#     @marshal_with(resource_fields)
-#     def put(self, video_id):
-#         args = video_put_args.parse_args()
-#         result = VideoModel.query.filter_by(id=video_id).first()
-#         if result:
-#             abort(409, message="Video id taken...")
-#         video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
-#         db.session.add(video)
-#         db.session.commit()
-#         return video, 201
-   
-#     #PUT (UPDATE in CRUD)
-#     @marshal_with(resource_fields)
-#     def patch(self, video_id):
-#         args = video_update_args.parse_args()
-#         result = VideoModel.query.filter_by(id=video_id).first()
-#         if not result:
-#             abort(404, message="Video doesn't exist, cannot update")
-
-#         if args['name']:
-#             result.name = args['name']
-#         if args['views']:
-#             result.views = args['views']
-#         if args['likes']:
-#             result.likes = args['likes']
-
-#         db.session.commit()
-
-#         return result, 200
-
-    #DELETE (DELETE in CRUD)
-    #def delete(self, video_id):
-        #abort_if_video_id_doesnt_exist(video_id)
-        #del videos[video_id]
-        #return '', 204
 
 class User(Resource):
     #GET (READ in CRUD)
--- functions.py
+++ functions.py
@@ -1,13 +1,122 @@
 # DOES NOT DO ANYTHING BUT PARSE DATA
 
-# GET ZIPCODES AND RETURN INFO
-def zipReturn(data):
-    return
-
-# GET COVID AND RETURN INFO
-def covidReturn(data):
-    return
-
 # GET MEDICARE AND RETURN INFO
 def medicareReturn(data):
     return
+
+# Fix whole states
+def FixWholeState(state):
+    if state == 'Alabama':
+        return "AL"
+    elif state == "Alaska":
+        return "AK"
+    elif state == "Arizona":
+        return "AZ"
+    elif state == "Arkansas":
+        return "AR"
+    elif state == "American Samoa":
+        return "AS"
+    elif state == "California":
+        return "CA"
+    elif state == "Colorado":
+        return "CO"
+    elif state == "Connecticut":
+        return "CT"
+    elif state == "Delaware":
+        return "DE"
+    elif state == "District of Columbia":
+        return "DC"
+    elif state == "Florida":
+        return "FL"
+    elif state == "Georgia":
+        return "GA"
+    elif state == "Guam":
+        return "GU"
+    elif state == "Hawaii":
+        return "HI"
+    elif state == "Idaho":
+        return "ID"
+    elif state == "Illinois":
+        return "IL"
+    elif state == "Indiana":
+        return "IN"
+    elif state == "Iowa":
+        return "IA"
+    elif state == "Kansas":
+        return "KS"
+    elif state == "Kentucky":
+        return "KY"
+    elif state == "Louisiana":
+        return "LA"
+    elif state == "Maine":
+        return "ME"
+    elif state == "Massachusetts":
+        return "MA"
+    elif state == "Michigan":
+        return "MI"
+    elif state == "Minnesota":
+        return "MN"
+    elif state == "Mississippi":
+        return "MS"
+    elif state == "Missouri":
+        return "MO"
+    elif state == "Montana":
+        return "MT"
+    elif state == "Nebraska":
+        return "NE"
+    elif state == "Nevada":
+        return "NV"
+    elif state == "New Hampshire":
+        return "NH"
+    elif state == "New Jersey":
+        return "NJ"
+    elif state == "New Mexico":
+        return "NM"
+    elif state == "New York":
+        return "NY"
+    elif state == "North Carolina":
+        return "NC"
+    elif state == "North Dakota":
+        return "ND"
+    elif state == "Northern Mariana Islands":
+        return "MP"
+    elif state == "Ohio":
+        return "OH"
+    elif state == "Oklahoma":
+        return "OK"
+    elif state == "Oregon":
+        return "OR"
+    elif state == "Pennsylvania":
+        return "PA"
+    elif state == "Puerto Rico":
+        return "PR"
+    elif state == "Rhode Island":
+        return "RI"
+    elif state == "South Carolina":
+        return "SC"
+    elif state == "South Dakota":
+        return "SD"
+    elif state == "Tennessee":
+        return "TN"
+    elif state == "Texas":
+        return "TX"
+    elif state == "Trust Territories":
+        return "TT"
+    elif state == "Utah":
+        return "UT"
+    elif state == "Vermont":
+        return "VT"
+    elif state == "Virginia":
+        return "VA"
+    elif state == "Virgin Islands":
+        return "VI"
+    elif state == "Washington":
+        return "WA"
+    elif state == "West Virginia":
+        return "WV"
+    elif state == "Wisconsin":
+        return "WI"
+    elif state == "Wyoming":
+        return "WY"
+    else:
+        return "NO STATE"
